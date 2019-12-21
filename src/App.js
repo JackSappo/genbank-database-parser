afc9fc7fa00d9fd16b2eaa39bb6a29f1f2e89a9a
Jack <jisapp02@gmail.com>
2019-12-21 16:56:34 -0500

    find and print values and indices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      dataString: ''
+      // TODO: hardcoded
+      matcher: 'ATATTAGG',
+      matches: []
     }
   }
 
@@ -15,16 +16,40 @@ class App extends Component {
     const { data } = await axios.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=30271926&rettype=fasta&retmode=xml')
 
     const dataString = parse(data);
+    const reg = new RegExp(this.state.matcher, 'g');
+    // var string = 'A1B1Y:A1B2Y:A1B3Y:A1B4Z:A1B5Y:A1B6Y:A1B7Y:A1B8Z:A1B9Y:A1B10Y:A1B11Y';
+    // var reg = /A[0-9]+B[0-9]+Y:A[0-9]+B[0-9]+Y/g;
+    const matches = []
+    let found = false
+    while (found = reg.exec(dataString)) {
+      matches.push({
+        value: found[0],
+        start: reg.lastIndex - found[0].length + 1,
+        end: reg.lastIndex
+      });
+        
+        // reg.lastIndex -= found[0].split(':')[1].length;
+    }
+
+    console.log('~= MATCHES', matches)
 
     this.setState({
-      dataString
+      matches
     })
   }
 
   render() {
+    const { matches } = this.state;
+
     return (
       <div className="App">
-        {this.state.dataString}
+        {matches.map((match, i) => {
+          return (
+            <div key={i}>
+              {match.value} {match.start} {match.end}
+            </div>
+          )
+        })}
       </div>
     );
   }
