8c6fffe8e4922cdfcf895ef01085ad73fb92d3f4
Jack <jisapp02@gmail.com>
2019-12-22 00:14:31 -0500

    tbody

--- src/UserInputs.js
+++ src/UserInputs.js
@@ -7,36 +7,38 @@ function UserInputs (props) {
     <div id="main">
       <div className="user-input-container">
         <table>
-          <tr>
-            <td className="label">
-              Database
-            </td>
-            <td>
-              <div className="user-input">
-                <input name="databaseName" value={databaseName} onChange={onChange}  />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td className="label">
-              ID
-            </td>
-            <td>
-              <div className="user-input">
-                <input name="databaseId" value={databaseId} onChange={onChange} />
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td className="label">
-              Regex Matcher
-            </td>
-            <td>
-              <div className="user-input">
-                <input name="matcher" value={matcher} onChange={onChange} />
-              </div>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="label">
+                Database
+              </td>
+              <td>
+                <div className="user-input">
+                  <input name="databaseName" value={databaseName} onChange={onChange}  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="label">
+                ID
+              </td>
+              <td>
+                <div className="user-input">
+                  <input name="databaseId" value={databaseId} onChange={onChange} />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="label">
+                Regex Matcher
+              </td>
+              <td>
+                <div className="user-input">
+                  <input name="matcher" value={matcher} onChange={onChange} />
+                </div>
+              </td>
+            </tr>
+          </tbody>
         </table>
         <div className="button-container">
           <button disabled={loading} onClick={handleClick} >
