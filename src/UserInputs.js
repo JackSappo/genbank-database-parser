dd256f8cb38aad500829499f2e313f9f817f1686
Jack <jisapp02@gmail.com>
2019-12-22 15:10:12 -0500

    no table for UserInputs

--- src/UserInputs.js
+++ src/UserInputs.js
@@ -6,40 +6,9 @@ function UserInputs (props) {
   return (
     <div id="main">
       <div className="user-input-container">
-        <table>
-          <tbody>
-            <tr>
-              <td className="label">
-                Database
-              </td>
-              <td>
-                <div className="user-input">
-                  <input name="databaseName" value={databaseName} onChange={onChange}  />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="label">
-                ID
-              </td>
-              <td>
-                <div className="user-input">
-                  <input name="databaseId" value={databaseId} onChange={onChange} />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="label">
-                Regex Matcher
-              </td>
-              <td>
-                <div className="user-input">
-                  <input name="matcher" value={matcher} onChange={onChange} />
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <UserInputRow label="Database" name="databaseName" value={databaseName} onChange={onChange}/>
+        <UserInputRow label="ID" name="databaseId" value={databaseId} onChange={onChange}/>
+        <UserInputRow label="Regex Matcher" name="matcher" value={matcher} onChange={onChange}/>
         <div className="button-container">
           <button disabled={loading} onClick={handleClick} >
             GO!
@@ -51,6 +20,19 @@ function UserInputs (props) {
       </div>
     </div>
   );
+}
+
+function UserInputRow(props) {
+  return (
+    <div className="user-input-row">
+      <div className="user-input-label">
+        {props.label}
+      </div>
+      <div className="user-input">
+        <input name={props.name} value={props.value} onChange={props.onChange}  />
+      </div>
+    </div>
+  )
 }
 
 export default UserInputs;
